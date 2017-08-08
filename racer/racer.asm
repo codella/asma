8ca970bebf65d718e73bb84f9511644ac40bcd99
Mauro Codella <mauro.codella@gmail.com>
2017-08-08 21:16:50 +0200

    Finish page 4

--- racer/racer.asm
+++ racer/racer.asm
@@ -102,4 +102,151 @@ main segment
     call word ptr [si]
     jmp cicle
   master endp
+  
+  check proc near
+    mov ah, 0bh
+    int 21h
+    ret
+  check endp
+  
+  cge_cur proc near
+    puch ax
+    push bx
+    push cx
+    push dx
+    mov ch, al
+    xor bh, bh
+    mov dh, pos_r
+    mov dl, pos_c
+    mov ah, 02h
+    int 10h
+    mov ah, 08h
+    int 10h
+    mov old_prop, ah
+    mov old_char, al
+    mov ah, 09h
+    mov cx, 0001h
+    mov al, ch
+    int 10h
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+  cge_cur endp
+  
+  in_a proc near
+    mov ah, 07h
+    int 21h
+    or al, al
+    je spe
+    xor bl, bl
+    ret
+  spe:
+    int 21h
+    mov bl, 0ffh
+    ret
+  int_a endp
+  
+  time_pause
+    push ax
+    push bx
+    push cx
+    push dx
+    pushf
+    xor ch, ch
+    mov cl, cyls
+  inni:
+    push cx
+    mov ah, 02ch
+    int 21h
+    mov cent_p, dl
+  tryy:
+    int 21h
+    cmp dl, cent_p
+    jb adding
+  uuli:
+    sub dl, cent_p
+    cmp dl, 05h
+    jb tryy
+    pop cx
+    loop inni
+    call w_speed ; prima decremetna e poi salta se diverso da zero
+    popf
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+  adding:
+  add dl, 064h
+  jmp uuli
+time_pause endp
+
+clear_s proc near
+  push ax
+  push bx
+  push cx
+  push dx
+  mov ah, 06h
+  mov bh, fill
+  xor cx, cx
+  mov dh, 24
+  mov dl, 39
+  int 10h
+  pop dx
+  pop cx
+  pop bx
+  pop ax
+  ret
+clear_s endp
+
+mov_cur proc near
+  push ax
+  push bx
+  push dx
+  xor bh, bh
+  mov ah, 02h
+  int 10h
+  pop dx
+  pop bx
+  pop ax
+  ret
+mov_cur endp
+
+exit proc near
+  call oldres
+  call dos_r
+exit endp
+
+dos_r proc near
+  int 20h
+dos_r endp
+
+su proc near
+  call load_prop
+  mov bl, pos_r
+  or bl, bl
+  je no3
+  dec bl
+  mov pos_r, bl
+  call out_road
+  or bh, bh
+  jo no3
+  call is_goal
+  or bl, bl
+  jne oik1
+  mov bl, cur
+  call cge_cur
+  ret
+no3:
+  call bad_exit
+oik1:
+  call good_exit
+su endp
+
+giu proc nead
+  call load_prop
+  push dx
+su endp
     
+  
