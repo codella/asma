c2198bffb999da12723c9684d9d35f40808d093c
Mauro Codella <mauro.codella@gmail.com>
2017-08-08 22:03:25 +0200

    Uppercase asm code

--- cga_test/cga_test.asm
+++ cga_test/cga_test.asm
@@ -1,28 +1,28 @@
-.model small
-.code
-  org 100h
-  master proc near
-    xor ah, ah
-    mov al, 04h
-    int 10h
-    mov ah, 0bh
-    xor bh, bh
-    mov bl, 11
-    int 10h
-    mov bh, 01h
-    mov bl, 00h
-    int 10h
-    xor bh, bh
-    mov ah, 0ch
-    mov al, 02h
-    mov cx, 160
-    mov dx, 100
-    int 10h
-    mov ah, 07h
-    int 21h
-    xor ah, ah
-    mov al, 03h
-    int 10h
-    int 20h
-  master endp
-end master
+.MODEL SMALL
+.CODE
+  ORG 100h
+  MASTER PROC NEAR
+    XOR AH, AH
+    MOV AL, 04h
+    INT 10h
+    MOV AH, 0Bh
+    XOR BH, BH
+    MOV BL, 11
+    INT 10h
+    MOV BH, 01h
+    MOV BL, 00h
+    INT 10h
+    XOR BH, BH
+    MOV AH, 0Ch
+    MOV AL, 02h
+    MOV CX, 160
+    MOV DX, 100
+    INT 10h
+    MOV AH, 07h
+    INT 21h
+    XOR AH, AH
+    MOV AL, 03h
+    INT 10h
+    INT 20h
+  MASTER ENDP
+END MASTER
